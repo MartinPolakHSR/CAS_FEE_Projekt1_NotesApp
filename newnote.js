79d3818cb607d38aa614059cdda4107ff8019d17
Martin Polak <martinpolak.mail@gmail.com>
2017-10-16 23:51:43 +0200

    editmode in same page

--- newnote.js
+++ newnote.js
@@ -1,4 +1,3 @@
-var params = getSearchParameters();
 
 var savedNotes = sessionStorage.getItem("notes");
 if (!savedNotes) {
@@ -7,55 +6,23 @@ if (!savedNotes) {
 }
 savedNotes = JSON.parse(savedNotes);
 
-$( document ).ready(function() {
 
 
-
-    if (params.id) {
-        showData(params.id);
-
-    }
-
-})
-
-
-
-
-
-
-
-function showData(id){
-
-   $("#title").val(savedNotes[id].title);
-   $("#description").val(savedNotes[id].description);
-   $("#untildate").val(savedNotes[id].untildate);
-
-};
-
-function CreateEditNote(id){
-
-    if (params.id) {
-        alert('TODO');
-
-    } else {
-        createNewNote();
-    }
-
-}
 
 
 function createNewNote(){
 
 
-
-
     var newNote = new Object();
     var notes;
 
-    newNote.id = getLastID();
+
+
+    newNote.id =  getLastID();
     newNote.title = $("#title").val();
     newNote.description = $("#description").val();
     newNote.untildate = $("#untildate").val();
+
 
     if(sessionStorage.notes)
     {
@@ -64,11 +31,8 @@ function createNewNote(){
         notes=[];
     }
 
-    notes.push(newNote);
-
-
-    sessionStorage.setItem('notes', JSON.stringify(notes));
-
+    savedNotes.push(newNote);
+    sessionStorage.setItem('notes', JSON.stringify(savedNotes));
     window.location.replace("index.html");
 
 
@@ -95,17 +59,4 @@ function getLastID(){
 };
 
 
-function getSearchParameters() {
-    var prmstr = window.location.search.substr(1);
-    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
-}
 
-function transformToAssocArray( prmstr ) {
-    var params = {};
-    var prmarr = prmstr.split("&");
-    for ( var i = 0; i < prmarr.length; i++) {
-        var tmparr = prmarr[i].split("=");
-        params[tmparr[0]] = tmparr[1];
-    }
-    return params;
-}
--- notes.js
+++ notes.js
@@ -25,22 +25,82 @@ function showNotes() {
 // Add the compiled html to the page
     $('.content-placeholder').html(theCompiledHtml);
 
-    $( 'a.edit' ).on( 'click', function() {
-        editNote( $( this ).data('id') );
-    });
 
 }
 
 
 
 
-function editNote(id){
-    $(location).attr('href','newnote.html?id='+id)
-    //window.location.replace("newnote.html");
-    console.log( 'hallo');
-    $("#title").val('Titel');
-    $("#description").val('Beschreibung');
-    $("#untildate").val('01.01.2017');
+
+function editNote(){
+
+    for (var i = 0; i < savedNotes.length; i++) {
+        if(params.id == savedNotes[i].id){
+            getNoteData(savedNotes[i],i);
+            break;
+        }
+    }
 };
 
 
+
+$( document ).ready(function() {
+
+    showNotes();
+
+    $( "#sortByFinishDate" ).on( "click", function() {
+        alert('ToDo SortByFinishData');
+    });
+
+    $( "#sortByCreatedDate" ).on( "click", function() {
+        alert('ToDo sortByCreatedDate')
+    });
+
+    $( "#sortByImportance" ).on( "click", function() {
+        alert('ToDo sortByImportance')
+    });
+
+    $( "#sortByFinished" ).on( "click", function() {
+        alert('ToDo sortByFinished')
+    });
+
+    $( 'a.editbutton' ).on( 'click', function() {
+
+        var id = $( this ).data('id');
+        $( 'div#note_'+id ).toggleClass('edit view');
+
+        if   ($( 'div#note_'+id ).hasClass('view')) {
+
+            editNote(id);
+        }
+
+
+            });
+
+
+
+
+})
+
+
+function editNote(id){
+
+    for (var i = 0; i < savedNotes.length; i++) {
+        if(id == savedNotes[i].id){
+            console.log(savedNotes[i]);
+            getNoteData(savedNotes[i],i);
+            console.log(savedNotes[i]);
+            break;
+        }
+    }
+
+    sessionStorage.setItem('notes', JSON.stringify(savedNotes));
+};
+
+
+function getNoteData (Note, id) {
+    Note.id =id;
+    Note.title = $("#title_"+id).val();
+    Note.description = $("#description_"+id).val();
+    Note.untildate = $("#untildate_"+id).val();
+}
